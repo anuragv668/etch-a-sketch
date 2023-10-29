6c98645d063265b14962dc2fcf628af4ce6ebdb1
anuragv668 <anuragvarma668@gmail.com>
2023-10-29 12:34:05 +0530

    add the container for the grid

---
+++ script.js
@@ -0,0 +1,8 @@
+const body = document.querySelector('body');
+const container = document.createElement('div');
+container.classList.add('container');
+
+
+
+
+body.insertBefore(container, body.firstChild);
