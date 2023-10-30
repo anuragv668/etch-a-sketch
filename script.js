4491ae9a55b34d430873e97236200d98cb21a156
anuragv668 <anuragvarma668@gmail.com>
2023-10-31 02:12:35 +0530

    add color change on hower to grid

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ for (let i = 0; i < 16; i++ ) {
   for (let j = 0; j < 16; j++) {
     let square = document.createElement('div');
     square.classList.add('square');
+    square.addEventListener('mouseover', () => {
+      square.style.backgroundColor = 'black';
+    });
     row.appendChild(square);
   }
   container.appendChild(row);
