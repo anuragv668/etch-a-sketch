362307fdcb7e66b39a073a8347814e509e8b5ed1
anuragv668 <anuragvarma668@gmail.com>
2023-10-30 19:55:40 +0530

    create grid of squares

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ const body = document.querySelector('body');
 const container = document.createElement('div');
 container.classList.add('container');
 
-
+for (let i = 0; i < 16; i++ ) {
+  let row = document.createElement('div')
+  row.classList.add('row');
+  for (let j = 0; j < 16; j++) {
+    let square = document.createElement('div');
+    square.classList.add('square');
+    row.appendChild(square);
+  }
+  container.appendChild(row);
+}
 
 
 body.insertBefore(container, body.firstChild);
