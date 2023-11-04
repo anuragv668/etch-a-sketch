dd2fc0668ec461eb91a35b686a451bae6fe87a19
anuragv668 <anuragvarma668@gmail.com>
2023-11-04 13:38:44 +0530

    add resizing and headings

--- script.js
+++ script.js
@@ -2,19 +2,47 @@ const body = document.querySelector('body');
 const container = document.createElement('div');
 container.classList.add('container');
 
-for (let i = 0; i < 16; i++ ) {
-  let row = document.createElement('div')
-  row.classList.add('row');
-  for (let j = 0; j < 16; j++) {
-    let square = document.createElement('div');
-    square.classList.add('square');
-    square.addEventListener('mouseover', () => {
-      square.style.backgroundColor = 'black';
-    });
-    row.appendChild(square);
+let gridsize = 16;
+
+const btn = document.createElement('button');
+btn.onclick = () => {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
   }
-  container.appendChild(row);
-}
+  gridsize = parseInt(window.prompt('Enter dimensions: '));
+  mkgrid(gridsize);
+};
+btn.innerText = 'change';
+body.insertBefore(btn, body.firstChild);
+
+const btnDesc = document.createElement('h3');
+btnDesc.innerText = 'Choose the grid dimensions';
+body.insertBefore(btnDesc, body.firstChild);
+
+const mkgrid = (gridDimensions) => {
+    
+  for (let i = 0; i < gridDimensions; i++ ) {
+    let row = document.createElement('div')
+    row.classList.add('row');
+    for (let j = 0; j < gridDimensions; j++) {
+      let square = document.createElement('div');
+      square.classList.add('square');
+      square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = 'blue';
+      });
+      row.appendChild(square);
+    }
+    container.appendChild(row);
+  }
+
+};
+
+mkgrid(gridsize);
 
 
-body.insertBefore(container, body.firstChild);
+body.insertBefore(container, body.lastChild);
+
+const title = document.createElement('h1');
+title.innerText = 'Etch A Sketch';
+body.insertBefore(title, body.firstChild);
+
